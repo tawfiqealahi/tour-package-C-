1a1cad58f571f6909206b70c6a0c606c533ab930
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-10-30 12:24:04 -0500

    11

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -9,7 +9,7 @@ const Services = () => {
             servicessssss
             <h1>servicessssssssssssssssssss</h1>
             
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae neque dolorum hic a dolorem commodi, nihil, consectetur excepturi perferendis aliquam corrupti cumque quibusdam, eveniet numquam fugiat magnam quidem officiis maiores perspiciatis voluptates sit tenetur aut odio ad. Vero rem nulla fuga pariatur repellat adipisci maiores sapiente nemo obcaecati debitis, in exercitationem unde ipsum, placeat autem perferendis reiciendis aliquid tempore eum enim accusamus necessitatibus quae. Unde omnis enim quidem. Fuga magni, sint voluptatem at doloribus omnis nihil tenetur quo officiis quas necessitatibus cupiditate commodi mollitia inventore maxime voluptas. Exercitationem aut qui eveniet vitae doloremque. Et dolore neque non inventore voluptatum fugiat!</p>
+            <p>Lorem ipsum dolor, voluptatum fugiat!</p>
         </div>
     );
 };

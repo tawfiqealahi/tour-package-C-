ac6a076a88113e45cf191a3f661318b458ca139d
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-10-30 12:23:09 -0500

    9

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -8,6 +8,9 @@ const Services = () => {
             <MenuBar></MenuBar>
             servicessssss
             <h1>servicessssssssssssssssssss</h1>
+            <h2>tawfiq e aqlahi</h2>
+            h3  tawfiwq kdfnjkdn 
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae neque dolorum hic a dolorem commodi, nihil, consectetur excepturi perferendis aliquam corrupti cumque quibusdam, eveniet numquam fugiat magnam quidem officiis maiores perspiciatis voluptates sit tenetur aut odio ad. Vero rem nulla fuga pariatur repellat adipisci maiores sapiente nemo obcaecati debitis, in exercitationem unde ipsum, placeat autem perferendis reiciendis aliquid tempore eum enim accusamus necessitatibus quae. Unde omnis enim quidem. Fuga magni, sint voluptatem at doloribus omnis nihil tenetur quo officiis quas necessitatibus cupiditate commodi mollitia inventore maxime voluptas. Exercitationem aut qui eveniet vitae doloremque. Et dolore neque non inventore voluptatum fugiat!</p>
         </div>
     );
 };
